724a29206ba8da3fd7da0abfc965c848d9650e00
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2022-03-07 09:06:11 -0600

    inicio eleventy pagina red

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+const moment = require("moment");
+const now = new Date();
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.setTemplateFormats("njk,html,md");
+    
+    eleventyConfig.addPassthroughCopy('src');
+    eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPassthroughCopy('fancybox');
+    eleventyConfig.addPassthroughCopy('super-sidebar');
+    eleventyConfig.addPassthroughCopy('admin');
+
+    eleventyConfig.addNunjucksFilter("limit", function(array, limit) {
+        return array.slice(0, limit);
+    });
+
+    eleventyConfig.addNunjucksFilter("limitPart", function(array, limit1, limit2) {
+        return array.slice(limit1, limit2);
+    });
+
+    eleventyConfig.addFilter("dateFormat", function(date, format) {
+        return moment(date).format(format);
+    });
+
+    eleventyConfig.addCollection('podcastsHighlighted', (collectionApi) => {
+        return collectionApi.getFilteredByTag('podcasts').filter((item) => {
+            return item.data.highlight == true;
+        });
+    });
+}
