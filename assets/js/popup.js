1738207caf40d4dead6a6ddafb5f93b49323e76e
unknown <josemarianoreyesh02@gmail.com>
2022-08-23 09:09:21 -0600

    trabajos en pop up

---
+++ assets/js/popup.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display="none";
+        },1000
+    )
+});
+
+document.querySelector("#close").addEventListener
+("click",function(){
+    document.querySelector(".popup").style.display = "none";
+});
+    
+              
