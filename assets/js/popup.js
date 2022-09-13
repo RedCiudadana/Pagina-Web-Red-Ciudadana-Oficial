46e41e69f050f20e075c358503ec8ddc4e196867
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-09-13 08:04:40 -0600

    loader red

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",function(){
     setTimeout(
         function open(event){
-            document.querySelector(".popup").style.display="none";
+            document.querySelector(".popup").style.display="block";
         },1000
     )
 });
