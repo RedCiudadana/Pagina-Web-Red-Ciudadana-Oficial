c951151a220813b3954174b7881c730d3061023c
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-09-04 09:07:19 -0600

    pop up de red listo

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,15 +1,12 @@
-window.addEventListener("load",function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".popup").style.display="none";
-        },1000
-    )
-});
-
-document.querySelector("#close").addEventListener
-("click",function(){
-    document.querySelector(".popup").style.display = "none";
-});
-    
-
-              
+window.addEventListener("load", function () {
+    // No necesitas el setTimeout, puedes mostrar la ventana emergente inmediatamente si lo deseas
+    document.querySelector("#open").addEventListener("click", function (event) {
+      event.preventDefault(); // Evita que el enlace realice su acción predeterminada
+      document.querySelector(".popup").style.display = "block";
+    });
+  
+    document.querySelector("#close").addEventListener("click", function () {
+      document.querySelector(".popup").style.display = "none";
+    });
+  });
+  
