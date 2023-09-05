2a0359b198d5c5cfc938c5345e6bf7b512467fb3
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-09-05 12:05:35 -0600

    arreglo de video de popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,12 +1,24 @@
 window.addEventListener("load", function () {
-    // No necesitas el setTimeout, puedes mostrar la ventana emergente inmediatamente si lo deseas
-    document.querySelector("#open").addEventListener("click", function (event) {
-      event.preventDefault(); // Evita que el enlace realice su acción predeterminada
-      document.querySelector(".popup").style.display = "block";
-    });
-  
-    document.querySelector("#close").addEventListener("click", function () {
+  // No necesitas el setTimeout, puedes mostrar la ventana emergente inmediatamente si lo deseas
+  document.querySelector("#open").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que el enlace realice su acción predeterminada
+
+    // Agrega &autoplay=1 al src del iframe al abrir el popup
+    const iframe = document.querySelector("iframe");
+    const src = iframe.getAttribute("src");
+    iframe.setAttribute("src", src + "&autoplay=1");
+
+    document.querySelector(".popup").style.display = "block";
+  });
+
+  document.querySelector("#close").addEventListener("click", function () {
+    // Pausa el video antes de cerrar el popup
+    const iframe = document.querySelector("iframe");
+    const player = new Vimeo.Player(iframe);
+
+    player.pause().then(function () {
+      // Oculta el popup después de pausar el video
       document.querySelector(".popup").style.display = "none";
     });
   });
-  
+});
